689fc016e3811d7c4f269c62ebbc8c6fbfc3487a
Pavel Miadzvedzeu <p.miadzvedzeu@sam-solutions.com>
2022-07-17 23:54:36 +0300

    Game restart after head meet borders or snake's tail

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <SFML/Graphics.hpp>
 #include <chrono>
 #include <thread>
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>       /* time */
+#include <stdlib.h>
+#include <time.h>
 #include <queue>
 #include <iostream>
 
@@ -13,7 +13,6 @@
 #define VRT_PIXEL_COUNT             (WINDOW_HEIGHT/PIXEL_SIZE)
 #define PIXEL_BORDER_THICKNESS      2
 #define DEF_SNAKE_LEN               3
-
 
 class Pixel : public sf::RectangleShape 
 {
@@ -33,8 +32,7 @@ public:
     {
         pixelX = x;
         pixelY = y;
-        pixelPosition.x = x;
-        pixelPosition.y = y;
+        pixelPosition = sf::Vector2i(x, y);
         setPosition(pixelX*PIXEL_SIZE + PIXEL_BORDER_THICKNESS, pixelY*PIXEL_SIZE + PIXEL_BORDER_THICKNESS);
     }
 
@@ -63,14 +61,28 @@ public:
 
 class SnakePixel : public Pixel 
 {
-public:
+private:
     sf::Vector2i direction = sf::Vector2i(-1, 0);
     sf::Vector2i newDirection = sf::Vector2i(-1, 0);
+    bool isHead=false;
+
+public:
+    void markAsHead() {
+        isHead = true;
+        setFillColor(sf::Color::Yellow);
+    }
 
     SnakePixel()
     {
         setOutlineColor(sf::Color::White);
         setFillColor(sf::Color::Red);
+    }
+
+    SnakePixel(sf::Vector2i direct)
+    {
+        setOutlineColor(sf::Color::White);
+        setFillColor(sf::Color::Red);
+        direction = newDirection = direct;
     }
 
     void move()
@@ -81,6 +93,15 @@ public:
         setPixelPosition(currntPosition + currentDirection);
         direction = newDirection;
     }
+
+    void setNewDirection(sf::Vector2i newDirect)
+    {
+        newDirection = newDirect;
+        if (isHead)
+            direction = newDirection;
+    }
+
+    sf::Vector2i getDirection() {return direction;}
 };
 
 class Food : public Pixel 
@@ -102,33 +123,43 @@ int main()
 {
     using namespace std::this_thread; // sleep_for, sleep_until
     using namespace std::chrono; // nanoseconds, system_clock, seconds
-    int tailLen = 0;
 
     srand (time(NULL));
 
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "The Snake");
 
-    std::queue <SnakePixel> snake;
-    for (int i = 0; i < DEF_SNAKE_LEN; i++)
-    {
-        SnakePixel snakePixel = SnakePixel();
-        snakePixel.setPixelPosition(HRZ_PIXEL_COUNT/2 + i, VRT_PIXEL_COUNT/2);
-        snake.push(snakePixel);
-    }
-    
+    int tailLen;
+    std::queue<SnakePixel> snake;
     BorderSquare borders[2 * (HRZ_PIXEL_COUNT+VRT_PIXEL_COUNT)];
-    for(int i=0; i<HRZ_PIXEL_COUNT; i++)
-    {
-        borders[HRZ_PIXEL_COUNT+i].setPixelPosition(i, VRT_PIXEL_COUNT - 1);
-        borders[i].setPixelPosition(i, 0);
-    }
-    for(int i=0; i<VRT_PIXEL_COUNT; i++)
-    {
-        borders[2*HRZ_PIXEL_COUNT + VRT_PIXEL_COUNT+i].setPixelPosition(HRZ_PIXEL_COUNT-1, i);
-        borders[2*HRZ_PIXEL_COUNT + i].setPixelPosition(0, i);
-    }
-
     Food food = Food();
+
+    auto initialize = [&] () {
+        tailLen = 0;
+        std::queue<SnakePixel> empty;
+        std::swap(snake, empty);
+        for (int i = 0; i < DEF_SNAKE_LEN; i++)
+        {
+            SnakePixel snakePixel = SnakePixel();
+            snakePixel.setPixelPosition(HRZ_PIXEL_COUNT/2 + i, VRT_PIXEL_COUNT/2);
+            snake.push(snakePixel);
+        }
+        snake.front().markAsHead();
+        for(int i=0; i<HRZ_PIXEL_COUNT; i++)
+        {
+            borders[HRZ_PIXEL_COUNT+i].setPixelPosition(i, VRT_PIXEL_COUNT - 1);
+            borders[i].setPixelPosition(i, 0);
+        }
+        for(int i=0; i<VRT_PIXEL_COUNT; i++)
+        {
+            borders[2*HRZ_PIXEL_COUNT + VRT_PIXEL_COUNT+i].setPixelPosition(HRZ_PIXEL_COUNT-1, i);
+            borders[2*HRZ_PIXEL_COUNT + i].setPixelPosition(0, i);
+        }
+        food = Food();
+
+        return 0;
+    };
+
+    initialize();
 
     while (window.isOpen())
     {
@@ -141,28 +172,16 @@ int main()
         for (size_t i = 0; i < 200; i++)
         {
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-            {
-                snake.front().direction = sf::Vector2i(0, -1);
-                snake.front().newDirection = sf::Vector2i(0, -1);
-            }
+                snake.front().setNewDirection(sf::Vector2i(0, -1));
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-            {
-                snake.front().direction = sf::Vector2i(0, 1);
-                snake.front().newDirection = sf::Vector2i(0, 1);
-            }
+                snake.front().setNewDirection(sf::Vector2i(0, 1));
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-            {
-                snake.front().direction = sf::Vector2i(-1, 0);
-                snake.front().newDirection = sf::Vector2i(-1, 0);
-            }
+                snake.front().setNewDirection(sf::Vector2i(-1, 0));
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-            {
-                snake.front().direction = sf::Vector2i(1, 0);
-                snake.front().newDirection = sf::Vector2i(1, 0);
-            }
+                snake.front().setNewDirection(sf::Vector2i(1, 0));
 
             sleep_for(nanoseconds(10));
             sleep_until(system_clock::now() + milliseconds(1));
@@ -180,25 +199,39 @@ int main()
             snake.push(snake.front());
             snake.pop();
             if (i < DEF_SNAKE_LEN+tailLen - 1)
-            {
-                snake.front().newDirection = snake.back().direction;
-            }
+                snake.front().setNewDirection(snake.back().getDirection());
             snake.back().move();
             window.draw(snake.back());
         }
+        window.draw(snake.front());
 
         if (food.isMeet(snake.front()))
         {
             food.relocate();
-            SnakePixel tail = SnakePixel();
-            tail.direction = snake.back().direction;
-            tail.newDirection = snake.back().direction;
-            tail.setPixelPosition(snake.back().getPixelPosition() - tail.direction);
+            SnakePixel tail = SnakePixel(snake.back().getDirection());
+            tail.setPixelPosition(snake.back().getPixelPosition() - tail.getDirection());
             snake.push(tail);
             window.draw(snake.back());
             tailLen++;
         }
+
         window.draw(food);
+
+        for (size_t i = 0; i < 2*(HRZ_PIXEL_COUNT+VRT_PIXEL_COUNT); i++)
+        {
+            if (borders[i].isMeet(snake.front()))
+                initialize();
+        }
+
+        SnakePixel head = snake.front();
+        for (size_t i = 0; i < DEF_SNAKE_LEN+tailLen; i++)
+        {
+            snake.push(snake.front());
+            snake.pop();
+            if (i != 0)
+                if (snake.back().isMeet(head))
+                    initialize();
+        }
 
         window.display();
     }
